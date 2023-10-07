13282d424cc158f2dfeed98c504be217ad67db09
Vihar <viharvihar18@gmail.com>
2023-10-07 17:57:42 +0530

    passing stage 3

--- app/main.py
+++ app/main.py
@@ -1,21 +1,43 @@
 import socket
 
 def main():
-    # You can use print statements as follows for debugging; they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    
     # Create a server socket and listen on localhost at port 4221
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     
-    # Accept a connection from a client (this will block until a client connects)
-    client, addr = server_socket.accept()
+    while True:  # Add an infinite loop to keep the server running
+        # Accept a connection from a client
+        client, addr = server_socket.accept()
+        
+        # Read data from the client (the HTTP request)
+        request = client.recv(1024).decode("utf-8")  # Decode the bytes to a string
+        
+        # Extract the path from the HTTP request
+        path = get_path_from_request(request)
+        
+        # Determine the response based on the path
+        response = ""
+        if path == "/":
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        
+        # Send the response to the client
+        client.sendall(response.encode("utf-8"))  # Encode the string to bytes
+        
+        # Close the connection
+        client.close()
+
+def get_path_from_request(request):
+    # Split the request into lines and extract the path from the first line
+    lines = request.split("\r\n")
+    if lines:
+        # The first line should look like "GET /index.html HTTP/1.1"
+        parts = lines[0].split(" ")
+        if len(parts) > 1:
+            return parts[1]
     
-    # Read data from the client (you can ignore it for now)
-    client.recv(1024)
-    
-    # Send an HTTP/1.1 200 OK response to the client
-    response = b"HTTP/1.1 200 OK\r\n\r\n"
-    client.sendall(response)
+    # If the path cannot be extracted, return a default value
+    return "/"
 
 if __name__ == "__main__":
     main()
