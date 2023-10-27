e7348849f359875b524c073d85c259f87b65a630
Vihar <viharvihar18@gmail.com>
2023-10-28 00:44:15 +0530

    fixed stage four

--- app/main.py
+++ app/main.py
@@ -1,4 +1,32 @@
 import socket
+import threading
+
+def handle_client(client):
+    # Read data from the client (the HTTP request)
+    request = client.recv(1024).decode("utf-8")  # Decode the bytes to a string
+    
+    # Extract the path from the HTTP request
+    path = get_path_from_request(request)
+    
+    # Determine the response based on the path
+    response = ""
+    if path == "/":
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n"
+    elif path == "/user-agent":
+        user_agent = get_user_agent_from_request(request)
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+    elif path.startswith("/echo/"):
+        random_string = path[6:]  # Extract the random string from the path
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}".format(len(random_string), random_string)
+    else:
+        # Respond with 404 Not Found for paths that do not match "/echo/" or "/user-agent"
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    
+    # Send the response to the client
+    client.sendall(response.encode("utf-8"))  # Encode the string to bytes
+    
+    # Close the connection
+    client.close()
 
 def main():
     # Create a server socket and listen on localhost at port 4221
@@ -8,31 +36,9 @@ def main():
         # Accept a connection from a client
         client, addr = server_socket.accept()
         
-        # Read data from the client (the HTTP request)
-        request = client.recv(1024).decode("utf-8")  # Decode the bytes to a string
-        
-        # Extract the path from the HTTP request
-        path = get_path_from_request(request)
-        
-        # Determine the response based on the path
-        response = ""
-        if path == "/":
-            response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n"
-        elif path == "/user-agent":
-            user_agent = get_user_agent_from_request(request)
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-        elif path.startswith("/echo/"):
-            random_string = path[6:]  # Extract the random string from the path
-            response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}".format(len(random_string), random_string)
-        else:
-            # Respond with 404 Not Found for paths that do not match "/echo/" or "/user-agent"
-            response = "HTTP/1.1 404 Not Found\r\n\r\n"
-        
-        # Send the response to the client
-        client.sendall(response.encode("utf-8"))  # Encode the string to bytes
-        
-        # Close the connection
-        client.close()
+        # Create a new thread to handle the client
+        client_thread = threading.Thread(target=handle_client, args=(client,))
+        client_thread.start()
 
 def get_path_from_request(request):
     # Split the request into lines and extract the path from the first line
